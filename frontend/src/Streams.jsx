e71c8447ad0f1dc8c0fdbc41c1e6a41d9ba583e9
Wredi <piotr.skora@spoko.pl>
2023-03-19 21:43:07 +0100

    example stream search

--- frontend/src/Streams.jsx
+++ frontend/src/Streams.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 
 import BoxStreamInfo from './BoxStreamInfo';
+import { useState } from 'react';
 
 import { getStreams } from './utils';
 
@@ -13,8 +14,48 @@ export async function loader() {
   return { activeStreams };
 }
 
+function generateExampleBoxStreamInfo() {
+  const data = [
+    { title: 'Live Coding with React', channelName: 'Reactify', userName: 'john_doe' },
+    { title: 'Playing Valorant', channelName: 'ValoFan', userName: 'jane_smith' },
+    { title: 'Chess Tournament Finals', channelName: 'ChessMaster', userName: 'peter_wong' },
+    { title: 'Creating Art on Stream', channelName: 'ArtisticMind', userName: 'sarah_baker' },
+    { title: 'Gaming Marathon for Charity', channelName: 'CharityGaming', userName: 'mark_johnson' },
+    { title: 'Cooking Show: Italian Cuisine', channelName: 'ChefNico', userName: 'nicole_adams' },
+    { title: 'Fitness Stream: Yoga', channelName: 'YogaWithMe', userName: 'julie_garcia' },
+    { title: 'Live Music Performance', channelName: 'MusicMania', userName: 'alex_smith' },
+    { title: 'Traveling to Japan', channelName: 'JourneyToJapan', userName: 'david_kim' },
+    { title: 'Speedrunning Super Mario Bros.', channelName: 'MarioFanatic', userName: 'lisa_tan' },
+    { title: 'Painting and Sketching', channelName: 'ArtLover', userName: 'tom_lee' },
+    { title: 'League of Legends Match', channelName: 'LolMaster', userName: 'harry_potter' },
+    { title: 'React Native Workshop', channelName: 'ReactNativeDev', userName: 'megan_carter' },
+    { title: 'Exploring Nature', channelName: 'NatureLover', userName: 'jack_white' },
+    { title: 'Gaming Talk Show', channelName: 'GameOn', userName: 'jason_fernandez' },
+    { title: 'Fashion and Beauty Stream', channelName: 'BeautyQueen', userName: 'amanda_clark' },
+  ];
+
+  const boxStreamInfoList = [];
+  for (let i = 0; i < 16; i++) {
+    const item = data[Math.floor(Math.random() * data.length)];
+    boxStreamInfoList.push(
+      <BoxStreamInfo
+        key={i}
+        title={item.title}
+        channelName={item.channelName}
+        userName={item.userName}
+        imgLink={`https://picsum.photos/180/120?random=${i}`}
+      />
+    );
+  }
+
+  return boxStreamInfoList;
+}
+const test = generateExampleBoxStreamInfo();
+
 export default function Streams() {
   const { activeStreams } = useLoaderData();
+  const [query, setQuery] = useState("");
+
   const displayStreams = activeStreams.map((stream, idx) => 
     <BoxStreamInfo
       key={idx} 
@@ -24,124 +65,18 @@ export default function Streams() {
       imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
     />
   );
+
+  let test2 = test.filter((stream) => stream.props.channelName.toLowerCase().includes(query.toLowerCase()));
   return (
     <div className='content'>
       <h1>Aktywne streamy</h1>
+      <div className='query-box'>
+        <label htmlFor="query">Szukaj:</label>
+        <input id="query" type="text" onChange={e => setQuery(e.target.value)} />
+      </div>
       <div className='streams'>
           <div className='streams-inner'>
-              {/* {displayStreams} */}
-              <BoxStreamInfo
-                key={0} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={1} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={2} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={3} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={4} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={5} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={6} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={7} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={8} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={9} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={10} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={11} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={12} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={13} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={14} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
-              <BoxStreamInfo
-                key={15} 
-                title="{stream.streamTitle} "
-                channelName="{stream.channelName} "
-                userName="{stream.channelName} "
-                imgLink="https://i.ytimg.com/vi/UPtoIy-oPWQ/maxresdefault.jpg"
-              />
+              {test2}
           </div>
       </div>
     </div>
