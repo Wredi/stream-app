48b4095190f0c14ba2d1b0130c7fe4c9ce1b1768
Wredi <piotr.skora@spoko.pl>
2023-04-05 23:58:01 +0200

    backend small refactor

--- api/webapi/urls.py
+++ api/webapi/urls.py
@@ -5,14 +5,18 @@ from . import views
 app_name = 'webapi'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('channel-data/', views.curr_user_channel_data, name='channel_data'),
-    path('stream-data/', views.curr_user_stream_data, name='stream_data'),
-    path('active-streams/', views.all_active_streams, name='all_streams'),
-    path('update-stream/', views.curr_user_stream_update, name='update_stream'),
-    path('update-channel/', views.curr_user_channel_update, name='update_channel'),
-    path('stream-data/<str:username>/', views.stream_data_by_username, name='stream_data_by_username'),
-    path('check-logged/', views.is_user_logged, name='is_user_logged'),
+    path('users/new/', views.new_user, name='new_user'),
+
+    path('post-session/', views.session, name='session'),
+    path('delete-session/', views.delete_session, name='delete_session'),
+    path('session/', views.is_user_logged, name='is_user_logged'),
+
+    path('users/me/channel/', views.curr_user_channel_data, name='channel_data'),
+    path('users/me/channel-update/', views.curr_user_channel_update, name='update_channel'),
+
+    path('users/me/stream/', views.curr_user_stream_data, name='stream_data'),
+    path('users/me/stream-update/', views.curr_user_stream_update, name='update_stream'),
+
+    path('streams/active/', views.all_active_streams, name='all_streams'),
+    path('users/<str:username>/stream', views.stream_data_by_username, name='stream_data_by_username'),
 ]
