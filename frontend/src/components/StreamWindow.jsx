f49e367405b9fd0dd7da6e53e77aae2e20f2846a
Wredi <piotr.skora@spoko.pl>
2023-04-09 01:23:34 +0200

    full volume working

--- frontend/src/components/StreamWindow.jsx
+++ frontend/src/components/StreamWindow.jsx
@@ -1,12 +1,12 @@
 import Hls from 'hls.js';
 import { useState } from 'react';
 import { useEffect, useRef } from 'react';
-import { FaPause, FaPlay } from 'react-icons/fa';
+import { FaPause, FaPlay, FaVolumeDown, FaVolumeMute, FaVolumeUp } from 'react-icons/fa';
 import { IconContext } from "react-icons";
 import '../css/StreamWindow.css';
 
 const config = {
-    debug: true,
+    // debug: true,
     enableWorker: true,
     liveSyncDurationCount: 3,
     liveMaxLatencyDurationCount: 10,
@@ -32,6 +32,7 @@ export default function StreamWindow({src}) {
 
     const [volume, setVolume] = useState(1);
     const [isPlaying, setIsPlaying] = useState(false);
+    const [isMuted, setIsMuted] = useState(false);
 
     useEffect(() => {
         const hls = hlsRef.current = new Hls(config);
@@ -44,7 +45,7 @@ export default function StreamWindow({src}) {
     }, []);
 
     const onPlayClick = () => {
-        const hls = hlsRef.current;
+        // const hls = hlsRef.current;
         // hls.loadSource('http://localhost:8080/hls/witaj.m3u8');
         // hls.attachMedia(videoElement.current);
 
@@ -57,21 +58,48 @@ export default function StreamWindow({src}) {
         setIsPlaying(!isPlaying);
     }
 
+    const onMuteClick = () => {
+        console.log("jajoc");
+        if(isMuted){
+            videoElement.current.volume = volume;
+        }else{
+            videoElement.current.volume = 0.0;
+
+        }
+        setIsMuted(!isMuted);
+    }
+
     const onVolumeChange = (e) => {
-        setVolume(e.target.value);
+        const v = e.target.value;
+        videoElement.current.volume = v;
+        if(v < 0.00000001){
+            console.log("beka jak rzeka!");
+            setIsMuted(true);
+        }else{
+            setIsMuted(false);
+        }
+        setVolume(v);
     }
 
     return (
         <div className='video-player'>
-            <div class="shadow"></div>
+            <div className='shadow'></div>
             <video ref={videoElement}></video>
             <div className='player-controls'>
-                <button onClick={onPlayClick} className='play-button'>
-                    <IconContext.Provider value={{ className: "shared-class", size: 18, color: "#ffffff" }}>
+                <IconContext.Provider value={{ className: "shared-class", size: 14, color: "#ffffff" }}>
+                    <button onClick={onPlayClick} className='play-button'>
                         {isPlaying ? <FaPause/> : <FaPlay/> }
-                    </IconContext.Provider>
-                </button>
-                <input type='range' min={0} max={1} value={volume} step={0.01} onChange={onVolumeChange} className='volume'/>
+                    </button>
+                </IconContext.Provider>
+
+                <IconContext.Provider value={{ className: "shared-class", size: 18, color: "#ffffff" }}>
+                    <div className='volume-box'>
+                        <button onClick={onMuteClick} className='mute-button'>
+                            {isMuted ? <FaVolumeMute/> : volume > 0.5 ? <FaVolumeUp/> : <FaVolumeDown/>}
+                        </button>
+                        <input type='range' min={0} max={1} value={volume} step={0.01} onChange={onVolumeChange} className='volume'/>
+                    </div>
+                </IconContext.Provider>
             </div>
         </div>
     );
